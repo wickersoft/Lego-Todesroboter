6c63e7cbfe2a3d7c0fb991e56967e303415c5883
wickersoft <wickersoft@online.de>
2018-10-29 09:43:34 +0100

    RobotState enum entfernt

--- src/dieter/hans/RobotTask.java
+++ src/dieter/hans/RobotTask.java
@@ -1,7 +1,23 @@
 package dieter.hans;
 
+import java.util.HashMap;
+
 public abstract class RobotTask {
 	
+	private final HashMap<Integer, RobotTask> transitions = new HashMap<>();
+	
+	public void edge(int returnCode, RobotTask neighbor) {
+		transitions.put(returnCode, neighbor);
+	}
+
 	public abstract int run();
 	
+	public RobotTask runAndGetNext() {
+		int code = run();
+		if(code == 0) {
+			return this;
+		} else {
+			return transitions.get(code);
+		}
+	}
 }
---
+++ src/dieter/hans/StateMachine.java
@@ -0,0 +1,10 @@
+package dieter.hans;
+
+public class StateMachine {
+
+	
+	
+	
+	
+	
+}
